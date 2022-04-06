b17795e16facdba28d07923e6117f0c0717957c1
Anand Palanisamy <96760059+AnandP-Tringapps@users.noreply.github.com>
2022-04-06 17:42:53 +0530

    Update script.js

--- HTML, CSS/Task-7 (Search Bar)/js/script.js
+++ HTML, CSS/Task-7 (Search Bar)/js/script.js
@@ -1,7 +1,7 @@
 function searchFunction() {
     var key = document.querySelector("#searchbox").value.toUpperCase();
     var mydishes = document.querySelectorAll(".box");
-    for (let i = 0; i < mydishes.length; i++) {
+    for of(let i = 0; i < mydishes.length; i++) {
         var inItems = mydishes[i].querySelector("h2").innerText.toUpperCase();
         if (inItems.indexOf(key) > -1) {
             mydishes[i].style.display = ""
